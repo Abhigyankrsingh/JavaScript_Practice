563b5d1373ee1120a40876d932b93a73ed977f47
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-12-03 20:09:33 +0530

    Code_Updated

---
+++ practice3.js
@@ -0,0 +1,21 @@
+// const person1 = "Abhi";
+
+// const gender1 = "male";
+
+
+// const person2 = "John";
+
+// const gender2 = "male";
+
+// const person3 = "priya";
+
+// const gender3 = "female";
+
+
+const users1 = {
+    firstName: "Abhi",
+    gender: "male"
+}
+
+
+console.log(users1["gender"])
